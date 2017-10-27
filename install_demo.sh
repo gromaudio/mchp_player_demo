a18ac0affe1f114ed40849b441634d814fcf422a
v.kuznetsov <v.kuznetsov.work@gmail.com>
2017-10-27 16:40:16 +0300

    Update binaries

--- install_demo.sh
+++ install_demo.sh
@@ -8,6 +8,7 @@ adb shell mkdir /system/app/SimplifiedMediaPlayer
 adb push ./system/app/SimplifiedMediaPlayer/SimplifiedMediaPlayer.apk /system/app/SimplifiedMediaPlayer/
 adb push ./system/bin/base_daemon /system/bin/
 adb push ./system/bin/aoap_streaming /system/bin/
+adb push ./system/bin/apple_streaming /system/bin/
 adb push ./system/etc/usb_audio_policy_configuration.xml /system/etc/
 
 adb reboot
